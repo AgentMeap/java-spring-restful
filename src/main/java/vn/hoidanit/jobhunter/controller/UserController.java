ccd05147ad478887f3386976ad68093e6b7dc223
AgentMeap <huudatnguyen15@gmail.com>
2024-10-16 00:08:53 +0700

    full crud video 32

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,8 +1,12 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +33,25 @@ public class UserController {
     public String deleteUser(@PathVariable("id") long id){
         this.userService.handleDeleteUser(id);
         return "ericUser";
+    }
+
+    //fetch user by id
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id){
+        return this.userService.fetchUserById(id);
+        
+    }
+
+    //fetch all users
+    @GetMapping("/user")
+    public List<User> getAllUser(){
+        return this.userService.fetchAllUser();
+    }
+        
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user){
+
+        User updateUser = this.userService.handleUpdateUser(user);
+        return updateUser;
     }
 }
