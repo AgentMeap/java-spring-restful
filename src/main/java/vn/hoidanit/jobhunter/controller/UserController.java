19718e2bd027e4171fcd37b9aefc9d21146b8524
AgentMeap <huudatnguyen15@gmail.com>
2024-10-15 23:29:15 +0700

    video 28

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,11 @@ public class UserController {
 
         User newUser = this.userService.handleCreateUser(postManUser);
         return newUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id){
+        this.userService.handleDeleteUser(id);
+        return "ericUser";
     }
 }
