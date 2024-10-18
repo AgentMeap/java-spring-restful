24182b443cf7e62905e89a3aba14727d84539867
AgentMeap <huudatnguyen15@gmail.com>
2024-10-18 22:01:53 +0700

    exception handling video 43

--- src/main/java/vn/hoidanit/jobhunter/service/error/IdInvalidException.java
+++ src/main/java/vn/hoidanit/jobhunter/service/error/IdInvalidException.java
@@ -1,7 +1,7 @@
 package vn.hoidanit.jobhunter.service.error;
 
 public class IdInvalidException extends Exception{
-    
+    // Constructor that accepts a message
     public IdInvalidException(String message){
         super(message);
     }
