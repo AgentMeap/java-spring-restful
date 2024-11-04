fd1e75e65050d590ef4424776d940951943a0e6a
AgentMeap <huudatnguyen15@gmail.com>
2024-11-04 23:37:20 +0700

    module resume

--- src/main/java/vn/hoidanit/jobhunter/domain/Job.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Job.java
@@ -1,6 +1,9 @@
 package vn.hoidanit.jobhunter.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -20,6 +24,7 @@ import vn.hoidanit.jobhunter.util.constant.LevelEnum;
 import java.time.Instant;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -38,8 +43,11 @@ public class Job {
     private String location;
     private double salary;
     private int quantity;
+    
+    @Enumerated(EnumType.STRING)
     private LevelEnum level;
 
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String description;
 
     private Instant startDate;
@@ -59,6 +67,9 @@ public class Job {
     @JoinTable(name = "job_skill", joinColumns = @JoinColumn(name = "job_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
     private List<Skill> skills;
 
+    @OneToMany( mappedBy = "job", fetch = FetchType.LAZY)
+    @JsonIgnore
+    List<Resume> resumes;
 
     @PrePersist
     public void handleBeforeCreate(){
